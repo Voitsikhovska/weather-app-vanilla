692ea9adace0aedaca189e139f9256a9c1dd8546
Voitsikhovska <111961494+Voitsikhovska@users.noreply.github.com>
2023-10-07 14:35:17 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,19 @@ function displayTemperarure(response) {
   cityElement.innerHTML = response.data.name;
 }
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let cityName = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 
-axios.get(apiUrl).then(displayTemperarure);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperarure);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
